87fe77ee5cf48de8b12c774dbb3f8d5e5fc3fae5
Trần Đình Quý <160567708+QuyTD-Dev@users.noreply.github.com>
2026-01-27 15:01:41 +0700

    feat(monster): add damage logic

---
+++ Assets/Scripts/Combat/DamageDealer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] private int damage = 20; // Sát thương gây ra
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Kiểm tra xem vật va chạm có phải là Quái không (thông qua Tag)
+        if (other.CompareTag("Enemy"))
+        {
+            // Tìm component MonsterHealth trên người con quái
+            MonsterHealth enemyHealth = other.GetComponent<MonsterHealth>();
+
+            // Nếu tìm thấy script máu, thì gọi hàm trừ máu
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+        }
+    }
+}

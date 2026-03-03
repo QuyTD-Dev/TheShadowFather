87bf93efb1b841059af878dfa4e25d1fba22f33a
NguyenHongAn12 <anltne0201@gmail.com>
2026-03-03 21:21:27 +0700

    Update feature shop

---
+++ Assets/Scripts/Shop/PlayerInventory.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [Header("Tài sản hiện có")]
+    public int gold = 500; // Cho sẵn 500 vàng để bạn dễ test việc mua sắm
+
+    [Header("Số lượng vật phẩm")]
+    public int hpPotion = 0;     // Bình Máu
+    public int mpPotion = 0;     // Bình Năng lượng
+    public int antidote = 0;     // Thuốc Giải độc
+    public int ragePotion = 0;   // Thuốc Cuồng nộ
+}
